37d9169360ccc743b71c8238adbbb067bb7f3a04
BUYA-GH <baka3737baka@gmail.com>
2022-05-23 10:09:48 +0900

    Added: Logo model

--- web/back/logo/models.py
+++ web/back/logo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+class Logo(models.Model) :
+    image = models.ImageField(upload_to = 'logos/')
+    uploader = models.ForeignKey(User, related_name='uploader', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0}".format(self.uploader)
--- web/back/logo/serializers.py
+++ web/back/logo/serializers.py
@@ -1,6 +1,5 @@
-# Create your views here.
-
 from rest_framework import serializers
 from .models import Logo
 
-class LogoSerializer(serializers.)
+class LogoSerializer(serializers.ModelSerializer):
+    pass
