8505767ad263ee095e5eae3335219119b4a50a03
Jeon Seung Yoon <baka3737baka@gmail.com>
2022-07-04 14:58:44 +0900

    remove: Remove `back` project
    cause of disappeared `settings.py`

--- web/back/logo/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Logo
-
-class LogoSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(use_url=True)
-
-    class Meta:
-        model = Logo
-        fields = ('image', 'uploader')
