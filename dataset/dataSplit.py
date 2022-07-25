dfe1bd435da7c6fed387f368435e4ebb3e52adb5
ydw98 <dorong842@gmail.com>
2022-07-25 13:55:59 +0900

    change: dataSplit code

--- dataset/dataSplit.py
+++ dataset/dataSplit.py
@@ -1,20 +1,23 @@
 import os
 import shutil
 import random
+from sklearn.model_selection import train_test_split
 
-dir_path = 'C:\\Users\\me\Downloads\\archive (1)\\LogoDet-3K'
+dir_path = 'C:\\Users\\me\\Desktop\\GA2022\\LogoDet-3K\\Det-A'
 dir_names = os.listdir(dir_path)
 dir_names
 
-#LogoDet-3K split
 imagefile_path = os.path.join(dir_path,dir_names[0])
 labelfile_path = os.path.join(dir_path,dir_names[1])
+
+"""
+#LogoDet-3K split
 file_names = os.listdir(imagefile_path)
 
-randomFileList = random.sample(file_names, 14000)
+randomFileList = random.sample(file_names, 100)
 for file in randomFileList:
     dstsource = 'C:\\Users\\me\\Desktop\\GA2022\\LogoDet-3K'
-    dstsource = os.path.join(dstsource, 'Det-C')
+    dstsource = os.path.join(dstsource, 'Det-A')
 
     #image
     src = os.path.join(imagefile_path,file)
@@ -22,24 +25,34 @@ for file in randomFileList:
     shutil.copy(src, dst)
 
     #label
-    file = file[:-4] + ".xml"
+    file = file[:-4] + ".txt"
     src = os.path.join(labelfile_path,file)
     dst = os.path.join(dstsource, 'labels')
     shutil.copy(src, dst)
-
+"""
 #train/val split
-for dir_name in dir_names:
-    file_path = os.path.join(dir_path,dir_name) + "/"
-    file_names = os.listdir(file_path)
-    for name in file_names:
-        src = os.path.join(file_path,name)
-        if os.path.isdir(src):
-            continue
-        number = name[4:-4]
-        if int(number)%5 ==0:
-            dst_path = file_path + "val/"
-            
-        else:
-            dst_path = file_path + "train/"
-        dst = os.path.join(dst_path,name)
-        os.rename(src,dst)
+image_file_name = [file_name for file_name in os.listdir(imagefile_path) if os.path.isfile(os.path.join(imagefile_path, file_name))]
+label_file_name = [file_name for file_name in os.listdir(labelfile_path) if os.path.isfile(os.path.join(labelfile_path, file_name))]
+image_train, image_test, label_train, label_test = train_test_split(image_file_name, label_file_name, test_size=0.2, random_state=121)
+
+for image in image_train:
+    src = os.path.join(imagefile_path,image)
+    dst = os.path.join(imagefile_path, "train")
+    dst = os.path.join(dst,image)
+    os.rename(src,dst)
+for image in image_test:
+    src = os.path.join(imagefile_path,image)
+    dst = os.path.join(imagefile_path, "val")
+    dst = os.path.join(dst,image)
+    os.rename(src,dst)
+for label in label_train:
+    src = os.path.join(labelfile_path, label)
+    dst = os.path.join(labelfile_path, "train")
+    dst = os.path.join(dst,label)
+    os.rename(src,dst)
+for label in label_train:
+    src = os.path.join(labelfile_path, label)
+    dst = os.path.join(labelfile_path, "val")
+    dst = os.path.join(dst,label)
+    os.rename(src,dst)
+#"""
