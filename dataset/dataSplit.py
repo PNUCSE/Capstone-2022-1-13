763e81afe3ba01073ffa3654f78b18b7fbec2148
ydw98 <dorong842@gmail.com>
2022-08-30 20:20:53 +0900

    Edit: code related to dataset creation

--- dataset/dataSplit.py
+++ dataset/dataSplit.py
@@ -2,13 +2,50 @@ import os
 import shutil
 import random
 from sklearn.model_selection import train_test_split
+from PIL import Image
+import numpy as np
+import cv2
 
-dir_path = 'C:\\Users\\me\\Desktop\\GA2022\\LogoDet-3K\\Det-D'
+
+dir_path = 'C:\\Users\\me\\Desktop\\GA2022\\LogoDet-3K\\Det-S_M_L_E'
 dir_names = os.listdir(dir_path)
 dir_names
 
 imagefile_path = os.path.join(dir_path,dir_names[0])
 labelfile_path = os.path.join(dir_path,dir_names[1])
+
+
+"""
+#cutout image create
+for image in os.listdir(imagefile_path):
+    image_path = os.path.join(imagefile_path, image)
+    cutout = Image.open(image_path)
+    cutout = np.asarray(cutout)
+    label_path = os.path.join(labelfile_path, image.replace('.jpg','.txt'))
+    label = open(label_path,'r')
+    label_list = label.readlines()
+    for line in label_list:
+        line = line.split()
+        x_center = float(line[1])*np.shape(cutout)[1]
+        y_center = float(line[2])*np.shape(cutout)[0]
+        width = float(line[3])*np.shape(cutout)[1]
+        height = float(line[4])*np.shape(cutout)[0]
+
+        x1=random.uniform(x_center-width/2,x_center+width/4) 
+        y1=random.uniform(y_center-height/2,y_center+height/4)
+        x_cutout = random.uniform(width/4 ,x_center+width/2-x1)
+        y_cutout = random.uniform(height/4 ,y_center+height/2-y1)
+        x2=x1+x_cutout
+        y2=y1+y_cutout
+        x1=int(x1)
+        x2=int(x2)
+        y1=int(y1)
+        y2=int(y2)
+        cutout = cv2.rectangle(cutout, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 0), cv2.FILLED)
+    cutout = Image.fromarray(cutout)
+    os.remove(image_path)
+    cutout.save(image_path)
+"""
 
 """
 #LogoDet-3K split
@@ -30,10 +67,11 @@ for file in randomFileList:
     dst = os.path.join(dstsource, 'labels')
     shutil.copy(src, dst)
 """
+
 #train/val split
-image_file_name = [file_name for file_name in os.listdir(imagefile_path) if os.path.isfile(os.path.join(imagefile_path, file_name))]
-label_file_name = [file_name for file_name in os.listdir(labelfile_path) if os.path.isfile(os.path.join(labelfile_path, file_name))]
-image_train, image_test, label_train, label_test = train_test_split(image_file_name, label_file_name, test_size=0.2, random_state=121)
+image_file_names = [file_name for file_name in os.listdir(imagefile_path) if os.path.isfile(os.path.join(imagefile_path, file_name))]
+label_file_names= [file_name for file_name in os.listdir(labelfile_path) if os.path.isfile(os.path.join(labelfile_path, file_name))]
+image_train, image_test, label_train, label_test = train_test_split(image_file_names, label_file_names, test_size=0.2, random_state=121)
 
 for image in image_train:
     src = os.path.join(imagefile_path,image)
@@ -55,4 +93,3 @@ for label in label_test:
     dst = os.path.join(labelfile_path, "val")
     dst = os.path.join(dst,label)
     os.rename(src,dst)
-#"""
--- dataset/rename.py
+++ dataset/rename.py
@@ -1,6 +1,7 @@
 import os
+import shutil
 
-dir_path = 'C:\\Users\\me\\Downloads\\archive (1)\\LogoDet-3K\\Electronic'#여기 경로 넣으면됨
+dir_path = 'C:\\Users\\me\\Downloads\\LogoDet-3K\\Electronic'#여기 경로 넣으면됨
 subdir_names = os.listdir(dir_path)
 subdir_names
 """
@@ -9,20 +10,23 @@ for subdir_name in subdir_names:
     dst = os.path.join("C:\\Users\\me\\Downloads\\archive (1)\\LogoDet-3K\\labels",subdir_name)
     os.rename(src,dst)
 """
-i=1
+i=13584
+
 for subdir_name in subdir_names:
     file_path = os.path.join(dir_path,subdir_name)
     file_names = os.listdir(file_path)
     for file_name in file_names:
         src = os.path.join(file_path,file_name)
         if(file_name[-3:]=="jpg"):
-            dst = "C:\\Users\\me\\Downloads\\archive (1)\\LogoDet-3K\\" +  "images"
+            dst = os.path.join("C:\\Users\\me\\Desktop\\GA2022\\LogoDet-3K\\Det-S_M_L_E","images")
             name = 'logo' + str(i) + ".jpg"
             dst = os.path.join(dst, name)
-            os.rename(src, dst)
+            #os.rename(src, dst)
+            shutil.copy(src,dst)
         elif(file_name[-3:]=="xml"):
-            dst = "C:\\Users\\me\\Downloads\\archive (1)\\LogoDet-3K\\" +  "labels"
+            dst = os.path.join("C:\\Users\\me\\Desktop\\GA2022\\LogoDet-3K\\Det-S_M_L_E", "labels")
             name = 'logo' + str(i) + '.xml'
             dst = os.path.join(dst, name)
             os.rename(src, dst)
             i=i+1
+print(i)
